ee5deca500a06420612d379aaa60bad60b83bcef
568j4 <144990819+568j4@users.noreply.github.com>
2023-11-02 16:49:19 +0900

    1.

--- main.c
+++ main.c
@@ -10,11 +10,12 @@ int main(int argc, char *argv[]) {
 	int grade[N_STUDENT];
 	int i;
 	
+	
 	for (i=0;i<N_STUDENT;i++)
-	  grade[i]=(i+1)*10;
+    	grade[i]=(i+1)*10;
 	  
 	for (i=0;i<N_STUDENT;i++)   
-	printf("grade[%i]=%i\n", i, grade[i]);
+    	printf("grade[%i]=%i\n", i, grade[i]);
 	
 	system("PAUSE");
 	return 0;
